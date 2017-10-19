c4cbe9db327f54b639719730e934f4e4ff31a96a
Tom Ashley <tomashley@protonmail.com>
2017-10-18 22:23:55 -0400

    Update restart-plasma.sh

--- kde5/restart-plasma.sh
+++ kde5/restart-plasma.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 pkill plasmashell
-nohup kstart plasmashell > /dev/null 2>&1&
+#nohup kstart plasmashell > /dev/null 2>&1&
+nohup plasmashell > /dev/null 2>&1&
+nohup kwin --replace > /dev/null 2>&1 &
